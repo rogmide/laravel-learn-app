b52a0383e77c0278f5841dc67e76f7fb8b0eb467
Roger Delgado <rogmide@gmail.com>
2023-09-12 11:32:44 -0400

    more learning

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -9,9 +9,13 @@ class Post
     public static function find($slug)
     {
         if (!file_exists($path = resource_path("posts/{$slug}.html"))) {
+
             throw new ModelNotFoundException();
         }
+        // var_dump($path);
         // caching stuff see the number 1200 represent the time that will be in cache
-        return cache()->remember("post.{slug}", 1200, fn () => file_get_contents($path));
+        return file_get_contents($path);
+        // the next line is giving me a error
+        // return cache()->remember("post.{slug}", 1200, fn () => file_get_contents($path));
     }
 }
