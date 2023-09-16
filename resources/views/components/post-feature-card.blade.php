2734be2f5cc254e0c19beeaed01c08df4729a7ee
Roger Delgado <rogmide@gmail.com>
2023-09-16 18:30:16 -0400

    adding css to project

--- resources/views/components/post-feature-card.blade.php
+++ resources/views/components/post-feature-card.blade.php
@@ -1,17 +1,18 @@
+@props(['post'])
 <article
     class="transition-colors duration-300 hover:bg-gray-100 border border-black border-opacity-0 hover:border-opacity-5 rounded-xl">
-    <div class="py-6 px-5">
-        <div>
-            <img src="./images/illustration-5.png" alt="Blog Post illustration" class="rounded-xl">
+    <div class="py-6 px-5 lg:flex">
+        <div class="flex-1 lg:mr-8">
+            {{-- TODO --}}
+            <img src="./images/illustration-1.png" alt="Blog Post illustration" class="rounded-xl">
         </div>
 
-        <div class="mt-8 flex flex-col justify-between">
-            <header>
+        <div class="flex-1 flex flex-col justify-between">
+            <header class="mt-8 lg:mt-0">
                 <div class="space-x-2">
                     <a href="/categories/{{ $post->category->slug }}"
                         class="px-3 py-1 border border-blue-300 rounded-full text-blue-300 text-xs uppercase font-semibold"
                         style="font-size: 10px">{{ $post->category->name }}</a>
-
                 </div>
 
                 <div class="mt-4">
@@ -20,12 +21,12 @@
                     </h1>
 
                     <span class="mt-2 block text-gray-400 text-xs">
-                        Published <time>{{ $post->created_at->diffForHumans() }}</time>
+                        Published <time>1 day ago</time>
                     </span>
                 </div>
             </header>
 
-            <div class="text-sm mt-4">
+            <div class="text-sm mt-2">
                 <p>
                     {{ $post->body }}
                 </p>
@@ -40,11 +41,10 @@
                     </div>
                 </div>
 
-                <div>
+                <div class="hidden lg:block">
                     <a href="/{{ $post->slug }}"
-                        class="transition-colors duration-300 text-xs font-semibold bg-gray-200 hover:bg-gray-300 rounded-full py-2 px-8">
-                        Read More
-                    </a>
+                        class="transition-colors duration-300 text-xs font-semibold bg-gray-200 hover:bg-gray-300 rounded-full py-2 px-8">Read
+                        More</a>
                 </div>
             </footer>
         </div>
