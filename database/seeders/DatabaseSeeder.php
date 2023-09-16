b2fc6544321221d010db3563c2f327f57c32eb87
Roger Delgado <rogmide@gmail.com>
2023-09-16 14:54:17 -0400

    custom seeding

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,60 +18,75 @@ class DatabaseSeeder extends Seeder
     {
 
         // clear the data in the db before seed it
+
         User::truncate();
         Post::truncate();
         Category::truncate();
 
-        $user = User::factory()->create();
-
-
-        /**
-         * Category Seeding START
-         */
-        // Sample how to seed the DB
-        Category::create([
-            'name' => 'Personal',
-            'slug' => 'personal'
+        // you can override fake properties by passing the 
+        // properties yourself
+        $user = User::factory()->create([
+            'name' => 'Roger Delgado'
         ]);
 
-        $family = Category::create([
-            'name' => 'Family',
-            'slug' => 'family'
+        // you can override fake properties by passing the 
+        // properties yourself
+        Post::factory(5)->create([
+            'user_id' => $user->id
         ]);
 
-        $work = Category::create([
-            'name' => 'Work',
-            'slug' => 'work'
-        ]);
-        /**
-         * Category Seeding END
-         */
-        /**
-         * Category Seeding START
-         */
+        // // THIS IS ONE WAY TO DO IT 
+        // // THE HARD WAY TO DO IT
+
+        // $user = User::factory()->create();
+
+        // /**
+        //  * Category Seeding START
+        //  */
+        // // Sample how to seed the DB
+        // Category::create([
+        //     'name' => 'Personal',
+        //     'slug' => 'personal'
+        // ]);
+
+        // $family = Category::create([
+        //     'name' => 'Family',
+        //     'slug' => 'family'
+        // ]);
+
+        // $work = Category::create([
+        //     'name' => 'Work',
+        //     'slug' => 'work'
+        // ]);
+        // /**
+        //  * Category Seeding END
+        //  */
+        // /**
+        //  * Category Seeding START
+        //  */
 
 
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $family->id,
-            'title' => 'My Family Post',
-            'slug' => 'my-family-post',
-            'excerpt' => '<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit </p>',
-            'body' => '<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. In cursus turpis massa tincidunt dui ut ornare lectus. Consectetur a erat nam at. Consectetur adipiscing elit duis tristique sollicitudin nibh sit amet. Diam sit amet nisl suscipit adipiscing bibendum est. </p>'
-        ]);
+        // Post::create([
+        //     'user_id' => $user->id,
+        //     'category_id' => $family->id,
+        //     'title' => 'My Family Post',
+        //     'slug' => 'my-family-post',
+        //     'excerpt' => '<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit </p>',
+        //     'body' => '<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. In cursus turpis massa tincidunt dui ut ornare lectus. Consectetur a erat nam at. Consectetur adipiscing elit duis tristique sollicitudin nibh sit amet. Diam sit amet nisl suscipit adipiscing bibendum est. </p>'
+        // ]);
 
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $work->id,
-            'title' => 'My Work Post',
-            'slug' => 'my-work-post',
-            'excerpt' => '<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit </p>',
-            'body' => '<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. In cursus turpis massa tincidunt dui ut ornare lectus. Consectetur a erat nam at. Consectetur adipiscing elit duis tristique sollicitudin nibh sit amet. Diam sit amet nisl suscipit adipiscing bibendum est. </p>'
-        ]);
+        // Post::create([
+        //     'user_id' => $user->id,
+        //     'category_id' => $work->id,
+        //     'title' => 'My Work Post',
+        //     'slug' => 'my-work-post',
+        //     'excerpt' => '<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit </p>',
+        //     'body' => '<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. In cursus turpis massa tincidunt dui ut ornare lectus. Consectetur a erat nam at. Consectetur adipiscing elit duis tristique sollicitudin nibh sit amet. Diam sit amet nisl suscipit adipiscing bibendum est. </p>'
+        // ]);
 
 
-        /**
-         * Category Seeding END
-         */
+        // /**
+        //  * Category Seeding END
+        //  */
     }
 }
