de5350cd13603b5f2a38bf7dc014c1590073d5bf
Roger Delgado <rogmide@gmail.com>
2023-09-20 08:52:17 -0400

    moving to form

--- resources/views/components/dropdown.blade.php
+++ resources/views/components/dropdown.blade.php
@@ -9,7 +9,7 @@
 
     {{-- Links --}}
 
-    <div x-show="show" class="py-2 absolute bg-gray-100 w-full rounded-xl mt-2 z-50">
+    <div x-show="show" class="py-2 absolute bg-gray-100 w-full rounded-xl mt-2 z-50 overflow-auto max-h-52">
         {{ $slot }}
     </div>
 
