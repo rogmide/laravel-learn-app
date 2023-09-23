1518cf97eee5e533dd7bbb39ccf99e439d0e45ac
Roger Delgado <rogmide@gmail.com>
2023-09-23 18:32:16 -0400

    cleaning up

---
+++ resources/views/components/submit-button.blade.php
@@ -0,0 +1,3 @@
+{{-- @props(['propName']) --}}
+<button class="bg-blue-500 font-semibold hover:bg-blue-800 px-10 py-2 rounded-2xl text-white text-xs uppercase mt-4"
+    type="submit">{{ $slot }}</button>
--- resources/views/posts/_add-comment-form.blade.php
+++ resources/views/posts/_add-comment-form.blade.php
@@ -21,9 +21,10 @@
 
 
             <div class="border-t-2 flex justify-end mt-6">
-                <button
-                    class="bg-blue-500 font-semibold hover:bg-blue-800 px-10 py-2 rounded-2xl text-white text-xs uppercase mt-4"
-                    type="submit">Post</button>
+                <x-submit-button>
+                    {{-- {{ $slot }} --}}
+                    Post
+                </x-submit-button>
             </div>
         </form>
     </x-panel>
