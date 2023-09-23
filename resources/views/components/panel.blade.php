2c48a70af16af364b3471078599adba97ede96c8
Roger Delgado <rogmide@gmail.com>
2023-09-23 14:27:30 -0400

    more laravel learning

---
+++ resources/views/components/panel.blade.php
@@ -0,0 +1,4 @@
+<div {{ $attributes(['class' => 'border rounded-xl border-gray-200 p-6']) }}>
+    {{-- Basacly $slot is whatever you put in between the tag of the component --}}
+    {{ $slot }}
+</div>
