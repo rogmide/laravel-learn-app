a677317e4d088bc6b4f66c0baac7579d71a1f60f
Roger Delgado <rogmide@gmail.com>
2023-09-23 11:05:21 -0400

    dynamic comming

---
+++ resources/views/components/post-comment.blade.php
@@ -0,0 +1,20 @@
+<article class="flex bg-gray-100 p-6 rounded-xl border-gray-200 space-x-4">
+    <div>
+        <img class="rounded-xl" src="https://i.pravatar.cc/300" alt="" />
+    </div>
+    <div>
+        <header class="mb-4">
+            <h3 class="font-bold"> Roger Delgado </h3>
+            <p class="text-xs">
+                <time>8 months ago</time>
+            </p>
+        </header>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+            incididunt
+            ut labore et dolore magna aliqua. Varius vel pharetra vel turpis nunc eget lorem.
+            Aenean
+            pharetra magna ac placerat vestibulum lectus mauris ultrices eros. Pharetra vel
+            turpis
+        </p>
+    </div>
+</article>
