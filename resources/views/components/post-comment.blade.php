bb07d282e88e1c2b5392c9336337357ecf10d5ce
Roger Delgado <rogmide@gmail.com>
2023-09-23 15:08:41 -0400

    working with comment and auth

--- resources/views/components/post-comment.blade.php
+++ resources/views/components/post-comment.blade.php
@@ -1,7 +1,7 @@
 @props(['comment'])
 <article class="flex bg-gray-100 p-6 rounded-xl border-gray-200 space-x-4">
     <div class="flex-shrink-0">
-        <img class="rounded-xl" src="https://i.pravatar.cc/60?u={{ $comment->id }}" alt="" width="60"
+        <img class="rounded-xl" src="https://i.pravatar.cc/60?u={{ $comment->user_id }}" alt="" width="60"
             height="60" />
     </div>
     <div>
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -64,30 +64,39 @@
                     </div>
                     <section class="col-span-8 col-start-5 mt-5 space-y-6">
 
-                        <x-panel>
-                            <form class="" method="POST" action="/posts/{{ $post->slug }}/comments">
-                                @csrf
-
-                                <header class="flex items-center">
-                                    <img class="rounded-full" src="https://i.pravatar.cc/50?u={{ auth()->id() }}"
-                                        alt="" width="50" height="50" />
-                                    <h2 class="ml-3"> What to participate?</h2>
-                                </header>
-
-                                <textarea name="body" class="p-3 m-2 w-full rounded-xl text-sm focus:outline-none focus:ring mt-4" cols="10"
-                                    rows="5" placeholder="Quick think something to say!"></textarea>
+                        @auth
 
 
+                            <x-panel>
+                                <form class="" method="POST" action="/posts/{{ $post->slug }}/comments">
+                                    @csrf
 
-                                <div class="border-t-2 flex justify-end mt-6">
-                                    <button
-                                        class="bg-blue-500 font-semibold hover:bg-blue-800 px-10 py-2 rounded-2xl text-white text-xs uppercase mt-4"
-                                        type="submit">Post</button>
-                                </div>
-                            </form>
-                        </x-panel>
+                                    <header class="flex items-center">
+                                        <img class="rounded-full" src="https://i.pravatar.cc/50?u={{ auth()->id() }}"
+                                            alt="" width="50" height="50" />
+                                        <h2 class="ml-3"> What to participate?</h2>
+                                    </header>
+
+                                    <textarea name="body" class="p-3 m-2 w-full rounded-xl text-sm focus:outline-none focus:ring mt-4" cols="10"
+                                        rows="5" placeholder="Quick think something to say!"></textarea>
 
 
+
+                                    <div class="border-t-2 flex justify-end mt-6">
+                                        <button
+                                            class="bg-blue-500 font-semibold hover:bg-blue-800 px-10 py-2 rounded-2xl text-white text-xs uppercase mt-4"
+                                            type="submit">Post</button>
+                                    </div>
+                                </form>
+                            </x-panel>
+                        @else
+                            <p class="font-semibold">
+                                <a class="hover:underline" href="/reg/register">Register</a> or <a class="hover:underline"
+                                    href="/reg/login">Log
+                                    in</a> to leave a comment.
+                            </p>
+
+                        @endauth
 
                         @foreach ($post->comments as $comment)
                             <x-post-comment :comment="$comment" />
