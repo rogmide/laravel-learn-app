0a74c579b0f733603d2ad470e87551c916695941
Roger Delgado <rogmide@gmail.com>
2023-09-25 15:08:52 -0400

    project inc changes

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Need to star working with API reques for project
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
